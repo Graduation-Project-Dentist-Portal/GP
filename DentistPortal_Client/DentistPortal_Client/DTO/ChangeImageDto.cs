eed06855e6172f795c01c62377d2b4c95cc22952
mookhaled <104018130+mookhaled@users.noreply.github.com>
2023-04-14 14:50:20 +0200

    mhmd client first update

---
+++ DentistPortal_Client/DentistPortal_Client/DTO/ChangeImageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DentistPortal_Client.DTO
+{
+    public class ChangeImageDto
+    {
+        public string username { get; set; }
+        public IFormFile? ProfilePicture { get; set; }
+        public IFormFile? IdentityCardPicture { get; set; }
+        public IFormFile? UniversityCardPicture { get; set; }
+    }
+}
---
+++ DentistPortal_Client/DentistPortal_Client/DTO/FullUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DentistPortal_Client.DTO
+{
+    public class FullUserDto
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string? ProfilePicture { get; set; } = String.Empty;
+        public bool Graduated { get; set; }
+        public string University { get; set; } = String.Empty;
+        public int Level { get; set; }
+    }
+}
