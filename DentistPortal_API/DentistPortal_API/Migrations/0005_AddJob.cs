fb091df1452ad0cf3febc77104986c03aaf24dc1
Haydimhmd <Haydiimohamedd@gmail.com>
2023-05-03 04:32:51 +0200

    Update
    Add Job Pages

--- DentistPortal_API/DentistPortal_API/Migrations/0005_AddJob.cs
+++ DentistPortal_API/DentistPortal_API/Migrations/0005_AddJob.cs
@@ -16,10 +16,13 @@ namespace DentistPortal_API.Migrations
                 .WithColumn("Id").AsGuid().NotNullable().PrimaryKey()
                 .WithColumn("JobTitle").AsString().NotNullable()
                 .WithColumn("Description").AsString().NotNullable()
-                .WithColumn("Salary").AsFloat().NotNullable()
+                .WithColumn("Salary").AsString().NotNullable()
                 .WithColumn("OwnerIdDoctor").AsGuid().Nullable().ForeignKey("Dentist", "Id").OnDelete(System.Data.Rule.Cascade)//Cant both be empty check at API
-                .WithColumn("OwnerIdPatient").AsGuid().Nullable().ForeignKey("Patient", "Id").OnDelete(System.Data.Rule.Cascade)//Cant both be empty check at API
-                .WithColumn("IsActive").AsBoolean().NotNullable();
+                .WithColumn("IsActive").AsBoolean().NotNullable()
+                .WithColumn("ContactEmail").AsString().NotNullable()
+                .WithColumn("ContactNumber").AsString().Nullable()
+                .WithColumn("Location").AsString().NotNullable()
+                .WithColumn("Duration").AsString().NotNullable();
         }
     }
 }
---
+++ DentistPortal_Client/DentistPortal_Client/Pages/DoctorPages/Jobs/DisplayJobs.cshtml.cs
@@ -0,0 +1,158 @@
+using DentistPortal_Client.DTO;
+using DentistPortal_API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Net.Mime;
+using System.Net.Http.Headers;
+using System.Net;
+
+
+namespace DentistPortal_Client.Pages.DoctorPages.Jobs
+{
+    public class DisplayJobsModel : PageModel
+    {
+        public Guid DoctorId;
+        public IConfiguration config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+               .AddEnvironmentVariables()
+               .Build();
+        private IHttpClientFactory _httpClient;
+        [TempData]
+        public string Msg { get; set; } = String.Empty;
+        [TempData]
+        public string Status { get; set; } = String.Empty;
+        public Job Job = new();
+        public List<Job> Jobs = new();
+        public JobDto JobDto { get; set; }
+
+
+
+        public DisplayJobsModel(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory;
+        }
+
+
+
+        public async Task OnGet()
+        {
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(HttpContext.Session.GetString("Token"));
+            DoctorId = Guid.Parse(jwt.Claims.First().Value);
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
+
+
+
+            try
+            {
+                var request = await client.GetStringAsync("/api/Display-All-Jobs");
+                if (request is not null)
+                {
+                    if (request.Length > 0)
+                    {
+                        var options = new JsonSerializerOptions
+                        {
+                            WriteIndented = true,
+                            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+                        };
+                        Jobs = JsonSerializer.Deserialize<List<Job>>(request, options);
+                    }
+                }
+                else
+                {
+                    Msg = request.ToString();
+                    Status = "error";
+                }
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+                Status = "error";
+            }
+        }
+
+
+        public async Task<IActionResult> OnPost(JobDto job)
+        {
+            var token = HttpContext.Session.GetString("Token");
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            job.OwnerIdDoctor = Guid.Parse(jwt.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value);
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var jsonCategory = JsonSerializer.Serialize(job);
+            var content = new StringContent(jsonCategory, Encoding.UTF8, "application/json");
+            var request = await client.PostAsync("/api/Add-Job", content);
+            if (request.IsSuccessStatusCode)
+            {
+                Msg = "Job added successfully !";
+                Status = "success";
+                return RedirectToPage("DisplayJobs");
+            }
+            else
+            {
+                Msg = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Status = "error";
+                return RedirectToPage("DisplayJobs");
+            }
+        }
+
+        public async Task<IActionResult> OnPostEdit(JobDto jobDto, Guid id)
+        {
+
+            var token = HttpContext.Session.GetString("Token");
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var jsonCategory = JsonSerializer.Serialize(jobDto);
+            var content = new StringContent(jsonCategory, Encoding.UTF8, "application/json");
+            var request = await client.PutAsync($"api/Edit-Job/{id}", content);
+
+            if (request.IsSuccessStatusCode)
+            {
+                Msg = "Edited successfully!";
+                Status = "success";
+                return RedirectToPage("/DoctorPages/Jobs/DisplayJobs");
+            }
+            else
+            {
+                Msg = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Status = "error";
+                return RedirectToPage("", new { id });
+            }
+        }
+
+        public async Task<IActionResult> OnPostDelete(Guid id)
+        {
+
+            var token = HttpContext.Session.GetString("Token");
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            var DoctorId = jwt.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var request = await client.DeleteAsync($"/api/Delete-Job/" + id);
+            if (request.IsSuccessStatusCode)
+            {
+                Msg = "Deleted successfully!";
+                Status = "success";
+                return RedirectToPage("/DoctorPages/Jobs/DisplayJobs");
+            }
+            else
+            {
+                Msg = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Status = "error";
+                return RedirectToPage("");
+            }
+        }
+    }
+
+
+}
